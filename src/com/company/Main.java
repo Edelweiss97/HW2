75103ff7b78a0e60a8375f6ac2ef9dc5a75e0711
Edelwess97 <edelweiss.monolith@gmail.com>
2021-08-29 00:36:26 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Вазген";
+        int age = 45;
+        int temp = 28;
+
+        Boolean isSunny = true;
+        if (age < 45) {
+            System.out.println("Можно идти гулять");
+        } if  (temp < -20 && temp > 30) {
+            System.out.println("Можно идти гулять");{
+            }
+        } if  (temp < 28) {
+            System.out.println("Можно идти гулять");
+        }
+    }
